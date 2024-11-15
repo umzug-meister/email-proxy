c5eff89eccc08900fc78ebce611454ab50d1d218
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2024-11-15 21:32:42 +0100

    update

--- build/middleware/jsonParser.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.jsonParser = void 0;
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-exports.jsonParser = jsonParser;
--- build/utils/logger.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var winston = require('winston');
-var logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
-});
-module.exports = logger;
